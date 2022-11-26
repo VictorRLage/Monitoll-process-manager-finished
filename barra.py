7cd590fb9fce3ea8e57a1fca451871f117b17ace
victor lage <victor.lage@sptech.school>
2022-11-26 11:52:31 -0300

    a

--- barra.py
+++ barra.py
@@ -1,11 +1,14 @@
 import math
 import time
+import colorama
 
 
-def progress_bar(progresso, total):
+def progress_bar(progresso, total, color=colorama.Fore.YELLOW):
     porcentagem = 100 * (progresso/float(total))
-    barra = '#' * int(porcentagem) + '-' *(100 - int(porcentagem))
-    print(f"\r|{barra}| {porcentagem:.2f}%", end="\r")
+    barra = '█' * int(porcentagem) + '-' *(100 - int(porcentagem))
+    print(color + f"\r|{barra}| {porcentagem:.2f}%", end="\r")
+    if progresso == total:
+        print(colorama.Fore.GREEN + f"\r|{barra}| {porcentagem:.2f}%", end="\r")
 
 
 numbers = [x * 5 for x in range(2000, 3000)]
