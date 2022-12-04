78bc33c669e70377738dc294e9c7338f861679d5
victor lage <victor.lage@sptech.school>
2022-12-04 16:27:44 -0300

    sh brandao

---
+++ Python/brandao.sh
@@ -0,0 +1,6 @@
+    echo "Startando um script"
+
+    while true
+    do
+        echo "Olá Brandão :)"
+    done
--- Python/brandao/brandao.py
+++
@@ -1,5 +0,0 @@
-import time
-
-while True:
-    print('Hello, Brandão!')
-    time.sleep(10)
